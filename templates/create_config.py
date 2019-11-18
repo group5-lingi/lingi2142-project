7ccb6a12350ac912758677a421af47bcbabb7561
namilus <mrmosuliman.msm@gmail.com>
2019-11-18 16:50:21 +0100

    generated templates based on auto

--- templates/create_config.py
+++ templates/create_config.py
@@ -4,48 +4,54 @@ import sys
 import json
 from mako.template import Template
 from argparse import FileType, ArgumentParser
+from pprint import pprint
 
 
 def main():
-    with open('conf.json') as json_file:
+    with open('./auto/conf.json') as json_file:
         data = json.load(json_file)
 
-        print("Creating start scripts")
-        template = Template(filename="start.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "_start", 'w') as f:
-                f.write(template.render(data=conf))
+        for pop in data["pops"]:                
+                print("Creating start scripts")
+                template = Template(filename="start.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "_start", 'w') as f:
+                        f.write(template.render(data=router))
+                        
 
-        print("Creating boot scripts")
-        template = Template(filename="boot.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "_boot", "w") as f:
-                f.write(template.render(data=conf))
+                print("Creating boot scripts")
+                template = Template(filename="boot.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "_boot", "w") as f:
+                        f.write(template.render(data=router))
+                        
+                
 
-        print("Creating ld.so.conf")
-        template = Template(filename="ld.so.conf.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "/ld.so.conf", "w") as f:
-                f.write(template.render(data=conf))
+                print("Creating ld.so.conf")
+                template = Template(filename="ld.so.conf.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "/ld.so.conf", "w") as f:
+                        f.write(template.render(data=router))
 
-        print("Creating OSPF config")
-        template = Template(filename="ospf6d.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "/" + conf['name'].lower() + "_ospf.conf", "w") as f:
-                f.write(template.render(data=conf))
-
-        print("Creating BGP config")
-        template = Template(filename="bgp.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "/" + conf['name'].lower() + "_bgp.conf", "w") as f:
-                f.write(template.render(data=conf))
+                print("Creating OSPF config")
+                template = Template(filename="ospf6d.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "/" + router['name'].lower() + "_ospf.conf", "w") as f:
+                        f.write(template.render(data=router))                        
 
 
-        print("Creating zebra config")
-        template = Template(filename="zebra.mako")
-        for conf in data:
-            with open("../isp/" + conf['name'] + "/" + conf['name'].lower() + "_zebra.conf", "w") as f:
-                f.write(template.render(data=conf))
+                print("Creating BGP config")
+                template = Template(filename="bgp.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "/" + router['name'].lower() + "_bgp.conf", "w") as f:
+                        f.write(template.render(data=router))
+
+
+                print("Creating zebra config")
+                template = Template(filename="zebra.mako")
+                for router in pop["routers"]:
+                    with open("../isp/" + router['name'] + "/" + router['name'].lower() + "_zebra.conf", "w") as f:
+                        f.write(template.render(data=router))
 
 
 main()
